79612995cb4131b988a3ef698cb0548bbd1f7f33
Anpo13211 <126756334+Anpo13211@users.noreply.github.com>
2023-10-28 11:42:12 +0900

    Add files via upload

--- 4rd_week/bptree.cpp
+++ 4rd_week/bptree.cpp
@@ -1,6 +1,8 @@
 #include "bptree.h"
 #include <vector>
 #include <sys/time.h>
+#include <algorithm>
+using namespace std;
 
 struct timeval
 cur_time(void)
@@ -80,7 +82,7 @@ insert_in_parent(NODE *n, int key, NODE* N_prime)
         R->key[0] = key;
         R->chi[0] = n;
         R->chi[1] = N_prime;
-        R->nkey = 1;
+        R->nkey = 1;  // Only one key in the new root
         R->isLeaf = false;
         n->parent = R;
         N_prime->parent = R;
@@ -88,59 +90,71 @@ insert_in_parent(NODE *n, int key, NODE* N_prime)
         return R;
     }
 
-    NODE* P = n->parent;
+    NODE *P = n->parent;
+    int i, j;
 
     // Check if parent has space for new pointer
     if (P->nkey < N-1) {
-        int pos;
-        for (pos = 0; pos <= P->nkey; pos++) {
-            if (P->chi[pos] == n) break;
-        }
-        for (int i = P->nkey; i > pos; i--) {
-            P->key[i] = P->key[i-1];
-            P->chi[i+1] = P->chi[i];
-        }
-        P->key[pos] = key;
-        P->chi[pos+1] = N_prime;
+        for (i = 0; i < P->nkey; i++) 
+            if (key < P->key[i]) 
+                break;
+
+        for (j = P->nkey; j > i; j--) {		
+            P->chi[j+1] = P->chi[j];
+            P->key[j] = P->key[j-1];
+        } 
+
+        P->key[i] = key;
+        P->chi[i+1] = N_prime;
         P->nkey++;
-        N_prime->parent = P;
+
         return P;
-    }
+    } else { 
+        NODE *T = new NODE();
+        j = 0;
 
-    // Handle the case where parent does not have space for new pointer
-    TEMP T;
-    for (int i = 0, j = 0; i < P->nkey; i++, j++) {
-        if (j == N-1) j++;
-        T.key[j] = P->key[i];
-        T.chi[j] = P->chi[i];
-    }
-    T.chi[N-1] = P->chi[N-1];
-    T.key[N-1] = key;
-    T.chi[N+1] = N_prime;
-    T.nkey = N;
+        for (i = 0; i < P->nkey; i++, j++) {
+            if (j == i && key < P->key[i]) {
+                T->key[j] = key;
+                T->chi[j+1] = N_prime;
+                j++;
+            }
 
-    P->nkey = 0;
-    int split_pos = (N + 1) / 2;
-    for (int i = 0; i < split_pos; i++) {
-        P->chi[i] = T.chi[i];
-        P->key[i] = T.key[i];
-        P->nkey++;
-    }
-    P->chi[split_pos] = T.chi[split_pos];
+            T->key[j] = P->key[i];
+            T->chi[j+1] = P->chi[i+1];
+        }
 
-    NODE* P_prime = new NODE();
-    for (int i = split_pos + 1, j = 0; i <= N; i++, j++) {
-        P_prime->chi[j] = T.chi[i];
-        P_prime->key[j] = T.key[i];
-        P_prime->nkey++;
-    }
-    P_prime->chi[N-split_pos] = T.chi[N+1];
+        if (j == P->nkey) {
+            T->key[j] = key;
+            T->chi[j+1] = N_prime;
+        }
 
-    for (int i = 0; i <= P_prime->nkey; i++) {
-        P_prime->chi[i]->parent = P_prime;
-    }
+        NODE* P_prime = new NODE();
+        int K_prime = T->key[(N+1)/2];
 
-    return insert_in_parent(P, T.key[split_pos], P_prime);
+        P->nkey = (N-1)/2;
+        P_prime->nkey = (N-1) - P->nkey;
+
+        for (i = 0; i < P->nkey; i++) {
+            P->key[i] = T->key[i];
+            P->chi[i] = T->chi[i];
+        }
+        P->chi[P->nkey] = T->chi[P->nkey];
+
+        for (i = 0, j = (N+1)/2; j < N; i++, j++) {
+            P_prime->key[i] = T->key[j];
+            P_prime->chi[i] = T->chi[j];
+        }
+        P_prime->chi[i] = T->chi[j];
+
+        for (i = 0; i <= P_prime->nkey; i++) {
+            P_prime->chi[i]->parent = P_prime;
+        }
+
+        delete T;
+
+        return insert_in_parent(P, K_prime, P_prime);
+    }
 }
 
 NODE *
@@ -163,53 +177,55 @@ insert(int key, DATA *data)
 	if (Root == NULL) {
 		leaf = alloc_leaf(NULL);
 		Root = leaf;
-	}
-  else {
-    leaf = find_leaf(Root, key);
-  }
-	if (leaf->nkey < (N-1)) {
-		insert_in_leaf(leaf, key, data);
-	}
-	else { 
+	} else leaf = find_leaf(Root, key);
+    
+	if (leaf->nkey < (N-1)) insert_in_leaf(leaf, key, data); 
+    else 
+    { 
         NODE *new_leaf = alloc_leaf(leaf->parent);
 
-        int temp_keys[N];
-        NODE* temp_child[N+1];
+        NODE *temp = new NODE();
+        temp->nkey = leaf->nkey;
+        int i, j;
 
         for (int i = 0; i < N - 1; i++) {
-            temp_keys[i] = leaf->key[i];
-            temp_child[i] = leaf->chi[i];
+            temp->key[i] = leaf->key[i];
+            temp->chi[i] = leaf->chi[i];
         }
-        temp_child[N-1] = leaf->chi[N-1];
+        temp->chi[N-1] = leaf->chi[N-1];
 
-        int i, j;
-        // 新しいキーが挿入されるべき位置を見つける
-        for (i = 0; i < N-1 && key >= temp_keys[i]; i++);
+        for (i = 0; i < N-1; i++) {
+            if (temp->key[i] == 0 || key < temp->key[i]) break;
+        }
         for (j = N-1; j > i; j--) {
-            temp_keys[j] = temp_keys[j-1];
-            temp_child[j+1] = temp_child[j];
+            temp->key[j] = temp->key[j-1];
+            temp->chi[j] = temp->chi[j-1];
         }
-        temp_keys[i] = key;
-        temp_child[i+1] = (NODE *)data;
+        temp->key[i] = key;
+        if (i == N-1) temp->chi[N-1] = (NODE *)data;
+        else temp->chi[i+1] = (NODE *)data;
 
+        temp->chi[N-1] = leaf->chi[N-1];
+        leaf->chi[N-1] = temp;
+        
         int split = (N+1)/2;
         leaf->nkey = split;
         for (i = 0; i < split; i++) {
-            leaf->key[i] = temp_keys[i];
-            leaf->chi[i] = temp_child[i];
+            leaf->key[i] = temp->key[i];
+            leaf->chi[i] = temp->chi[i];
         }
-        leaf->chi[split] = temp_child[split];
+        leaf->chi[split] = temp->chi[split];
 
         new_leaf->nkey = N - split;
         for (i = split, j = 0; i < N; i++, j++) {
-            new_leaf->key[j] = temp_keys[i];
-            new_leaf->chi[j] = temp_child[i];
+            new_leaf->key[j] = temp->key[i];
+            new_leaf->chi[j] = temp->chi[i];
         }
-        new_leaf->chi[j] = temp_child[i];
+        new_leaf->chi[j] = temp->chi[i];
 
-        // Insert the middle key into the parent
-        int middle_key = new_leaf->key[0];
-        insert_in_parent(leaf->parent, middle_key, new_leaf);
+        int* ptr_to_smallest = min_element(new_leaf->key, new_leaf->key + new_leaf->nkey);
+        int smallest = *ptr_to_smallest;
+        insert_in_parent(leaf, smallest, new_leaf);
 	}
 }
 
@@ -239,10 +255,10 @@ main(int argc, char *argv[])
 
 	printf("-----Insert-----\n");
 	begin = cur_time();
-  while (true) {
+    while (true) {
 		insert(interactive(), NULL);
     	print_tree(Root);
-  }
+    }
 	end = cur_time();
 
 	return 0;
