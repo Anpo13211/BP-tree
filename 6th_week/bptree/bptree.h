a37192ad0a66d840c0d258bad573a2dbcaeeebdd
Anpo13211 <126756334+Anpo13211@users.noreply.github.com>
2025-01-04 16:48:50 +0900

    Update bptree.h

--- 6th_week/bptree/bptree.h
+++ 6th_week/bptree/bptree.h
@@ -1,3 +1,7 @@
+#ifndef BPTREEE_H
+#define BPTREEE_H
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +11,7 @@
 #include <math.h>
 #include "debug.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 #define MAX_OBJ (1000*1000)
 #define N 4
@@ -32,7 +37,12 @@ typedef struct _TEMP {
 	int nkey;
 } TEMP;
 
-DATA Head;
-DATA *Tail;
+extern NODE *Root;
+extern DATA Head;
+extern DATA *Tail;
 
-NODE *Root = NULL;
+void init_root();
+void insert(int key, DATA *data);
+NODE* find_leaf(NODE *root, int key);
+void print_tree(NODE *root);
+vector<int> range_query(int start, int end);
