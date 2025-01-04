1aeed73fc1da66c6d4bba5f1979126dc05d9c5ec
Anpo13211 <126756334+Anpo13211@users.noreply.github.com>
2025-01-04 16:41:41 +0900

    Update deploy.sh
    Add changes in order to commit successfully.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,33 @@
 #!/bin/sh -ex 
-target_branch="Anpo13211-upload-1" 
-git config --global user.name "CircleCI deployer" 
-git config --global user.email "<>" 
-git checkout $target_branch 
-git reset --hard origin/main 
+cd "$(dirname "$0")" # ルートディレクトリへ移動
 
+target_branch="Anpo13211-upload-1"
+
+git config --global user.name "CircleCI deployer"
+git config --global user.email "<>"
+
+git fetch origin
+git checkout $target_branch
+git reset --hard origin/main
+
+# Compile the source files
 gcc -o a.out 6th_week/bptree_test.cpp 6th_week/bptree.cpp -DTEST
-echo "output of a.out: $(./a.out)" > a.txt 
+if [ $? -ne 0 ]; then
+    echo "Compilation failed"
+    exit 1
+fi
 
-git add a.out a.txt 
-git commit -m "[skip ci] updates GitHub Pages" 
-if [ $? -ne 0 ]; then 
-    echo "nothing to commit" 
-    exit 0 
-fi 
-git remote set-url origin "git@github.com:Anpo13211/BP-tree.git" 
+# Run the compiled binary and save output
+echo "output of a.out: $(./a.out)" > a.txt
+
+# Stage and commit changes
+git add a.out a.txt
+git commit -m "[skip ci] updates GitHub Pages"
+if [ $? -ne 0 ]; then
+    echo "nothing to commit"
+    exit 0
+fi
+
+# Push changes to the remote branch
+git remote set-url origin "git@github.com:Anpo13211/BP-tree.git"
 git push -f origin $target_branch
